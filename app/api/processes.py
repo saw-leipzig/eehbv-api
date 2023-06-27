082c88a5dc2efd435fae27f6627b66cf5c1a1655
Markus Wittenberg <wittenberg@saw-leipzig.de>
2023-06-28 00:51:37 +0200

    update process model

--- app/api/processes.py
+++ app/api/processes.py
@@ -110,6 +110,121 @@ def create_process():
     return Response(json.dumps(new_process.as_dict()), mimetype='application/json')
 
 
+@api.route('/processes<int:cId>', methods=['PUT'])
+@permission_required(Permission.OPT)
+def update_process(cId):
+    proc = request.get_json()
+    old_p = get_process_raw(cId)
+    for key in ['view_name', 'api_name', 'variant_tree']:
+        setattr(old_p, key, proc[key])
+    funcs = {}
+    old_funcs = LossFunctions.query.filter_by(processes_id=cId)
+    func_ids = [func['id'] for func in proc['functions'] if 'id' in func]
+    for old_func in old_funcs:
+        if old_func.id not in func_ids:
+            db.session.delete(old_func)
+    old_params = ProcessParameters.query.filter_by(processes_id=cId)
+    param_ids = [param['id'] for param in proc['process']['process_parameters'] if 'id' in param]
+    for old_param in old_params:
+        if old_param.id not in param_ids:
+            db.session.delete(old_param)
+    old_vars = Variants.query.filter_by(processes_id=cId)
+    vars_ids = [v['id'] for v in proc['variants'] if 'id' in v]
+    for old_var in old_vars:
+        if old_var.id not in vars_ids:
+            db.session.delete(old_var)
+    for func in proc['functions']:
+        if 'id' in func:
+            for old_func in old_funcs:
+                if old_func.id == func['id']:
+                    for key in ['func', 'description', 'doc']:
+                        setattr(old_func, key, func[key])
+            funcs[func['description']] = func['id']
+        else:
+            func_dict = {**func, 'processes_id': cId}
+            f = LossFunctions(**func_dict)
+            db.session.add(f)
+            db.session.commit()
+            funcs[f.description] = f.id
+    for param in proc['process']['process_parameters']:
+        if 'id' in param:
+            for old_param in old_params:
+                if old_param.id == param['id']:
+                    for key in ['name', 'unit', 'variable_name', 'material_properties_id', 'defaults', 'general']:
+                        setattr(old_param, key, param[key])
+        else:
+            param_dict = {**param, 'processes_id': cId}
+            pp = ProcessParameters(**param_dict)
+            db.session.add(pp)
+    for variant in proc['variants']:
+        if 'id' in variant:
+            v_id = variant['id']
+            for old_var in old_vars:
+                if old_var.id == variant['id']:
+                    setattr(old_var, 'name', variant['name'])
+        else:
+            var_dict = {'name': variant['name'], 'processes_id': cId}
+            v = Variants(**var_dict)
+            db.session.add(v)
+            db.session.commit()
+            v_id = v.id
+        old_comps = VariantComponents.query.filter_by(variants_id=v_id)
+        for component in variant['variant_components']:
+            if 'id' in component:
+                for old_comp in old_comps:
+                    if old_comp.id == component['id']:
+                        for key in ['position', 'component_api_name', 'variable_name', 'description', 'aggregate']:
+                            setattr(old_comp, key, component[key])
+            else:
+                comp_dict = {**component, 'variants_id': v_id}
+                c = VariantComponents(**comp_dict)
+                db.session.add(c)
+        old_lfs = VariantsLossFunctions.query.filter_by(variants_id=v_id)
+        for lf in variant['variant_functions']:
+            needed_lf_entries = {key: lf[key] for key in ['parameter_list', 'variable_name', 'description', 'eval_after_position', 'position', 'aggregate', 'is_loss']}
+            if 'id' in lf:
+                for old_lf in old_lfs:
+                    if old_lf.id == lf['id']:
+                        for key in needed_lf_entries:
+                            setattr(old_lf, key, lf[key])
+            else:
+                lf_dict = {**needed_lf_entries, 'variants_id': v_id, 'loss_functions_id': funcs[lf['loss_function_description']]}
+                loss_func = VariantsLossFunctions(**lf_dict)
+                db.session.add(loss_func)
+        old_res = Restrictions.query.filter_by(variants_id=v_id)
+        for r in variant['variant_restrictions']:
+            needed_r_entries = {key: r[key] for key in ['description', 'eval_after_position', 'restriction']}
+            if 'id' in r:
+                for old_re in old_res:
+                    if old_re.id == r['id']:
+                        for key in needed_r_entries:
+                            setattr(old_re, key, r[key])
+            else:
+                r_dict = {**needed_r_entries, 'variants_id': v_id}
+                restr = Restrictions(**r_dict)
+                db.session.add(restr)
+        db.session.commit()
+    old_vs = VariantSelection.query.filter_by(processes_id=cId).first()
+    setattr(old_vs, 'selection', json.dumps(proc['variant_selection']))
+    old_solver = ProblemType.query.filter_by(processes_id=cId).first()
+    setattr(old_solver, 'code', proc['solver']['code'])
+    setattr(old_solver, 'use_solver', proc['solver']['use_solver'])
+    old_its = InfoTexts.query.filter_by(processes_id=cId)
+    for info_text in proc['infoTexts']:
+        for old_it in old_its:
+            if old_it.id == info_text['id']:
+                setattr(old_it, 'text', info_text['text'])
+    db.session.commit()
+    problem_dict.remove_function(cId)
+    loss_function_dict.remove_functions(cId)
+    process_source = {'processes_id': cId, 'request': json.dumps(proc)}
+    ps = ProcessSources(**process_source)
+    db.session.add(ps)
+    db.session.commit()
+    new_process = get_process_raw(cId)
+    return Response(json.dumps(new_process.as_dict()), mimetype='application/json')
+
+
 @api.route('/processes/<int:cId>/full')
 def get_process_details(cId):
     process = get_process_raw(cId)
@@ -119,6 +234,7 @@ def get_process_details(cId):
                     'functions': [lf.as_dict() for lf in process.loss_functions],
                     'variants': [
                         {'name': v.name,
+                         'id': v.id,
                          'variant_components': [vc.as_dict() for vc in v.variant_components],
                          'variant_functions': [{**vlf.as_dict(),
                                                 'loss_function_description': next(lf.description for lf in process.loss_functions
@@ -138,7 +254,7 @@ def get_process_details(cId):
                                                   for vr in v.restrictions]}
                         for v in variants
                     ],
-                    'variant_selection': json.loads(process.variant_selection[0].selection),
+                    'variant_selection': json.loads(process.variant_selection[0].selection) | {'id': process.variant_selection[0].id},
                     'solver': [s.as_dict() for s in process.process_solvers],
                     'infoTexts': [it.as_dict() for it in process.info_texts]})
 
